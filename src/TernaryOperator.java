4ba274810e62073a1a054f5e907568d3a8bb38f8
Eska <eskapdk@gmail.com>
2023-05-26 17:35:38 +0700

    posting awal

---
+++ src/TernaryOperator.java
@@ -0,0 +1,11 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 75;
+        var nilai2 = 70;
+        String ucapan = nilai >= 75 ? "Selamat Anda Lulus" : "Silahkan Coba lagi";
+        String ucapan2 = nilai2 >= 75 ? "Selamat Anda Lulus" : "Silahkan Coba lagi";
+
+        System.out.println(ucapan);
+        System.out.println(ucapan2);
+    }
+}
