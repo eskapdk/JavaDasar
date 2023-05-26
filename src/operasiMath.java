b97775ef3abc244d526aa1b96753a57ababca004
Eska <eskapdk@gmail.com>
2023-05-26 16:22:53 +0700

    posting awal

---
+++ src/operasiMath.java
@@ -0,0 +1,37 @@
+public class operasiMath {
+    public static void main(String[] args) {
+        
+    int a = 15;
+    int b = 20;
+
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+    System.out.println("======================");
+
+        //Augmented Assignments
+    int c = 10;
+
+    c += 4;//10+4=14
+    System.out.println(c);
+
+    c -= 2;//14-2=12
+    System.out.println(c);
+
+    c *= 10;//12*10=120
+    System.out.println(c);
+        System.out.println("===============");
+
+    int d = 8;
+
+    d++;//8+1=9
+    System.out.println(d);
+
+    d--;//9-1=8
+    System.out.println(d);
+
+
+    }
+}
